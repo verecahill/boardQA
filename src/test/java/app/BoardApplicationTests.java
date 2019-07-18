04c9f5113e6f5ecfc916314e55f97720448a806b
woojin <verecahill@gmail.com>
2019-07-18 20:43:36 +0900

    update BoardApplication.java

--- src/test/java/app/BoardApplicationTests.java
+++ src/test/java/app/BoardApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BoardQaApplicationTests {
+public class BoardApplicationTests {
 
 	@Test
 	public void contextLoads() {
