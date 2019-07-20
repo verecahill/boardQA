a1899a627edba57c2f96efe4c9d13cfde5f883f9
woojin <verecahill@gmail.com>
2019-07-20 11:24:42 +0900

    JPA ,REST URL, H2-Database added

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -3,28 +3,59 @@ package app.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import app.dto.User;
+import app.repository.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	private List<User> users = new ArrayList<User>();
+	
+	@Autowired
+	private UserRepository userRepository;
 
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user) {
 //		System.out.println(user);
-		users.add(user);
-		return "redirect:list";
+//		users.add(user);
+		userRepository.save(user);
+		return "redirect:users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users",users);
-		return "list";
+//		model.addAttribute("users",users);
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("form")
+	public String form() {
+	
+		return "/user/form";
+	}
+	
+	@GetMapping("{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("user", userRepository.findOne(id));
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
--- src/main/java/app/dto/User.java
+++ src/main/java/app/dto/User.java
@@ -1,11 +1,29 @@
 package app.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable=false)
 	private String userId;
+
+	@Column(nullable=false)
 	private String password;
+	
 	private String email;
 	private String name;
+	
 	public String getUserId() {
 		return userId;
 	}
@@ -33,6 +51,13 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", email=" + email + ", name=" + name + "]";
+	}
+	public void update(User newUser) {
+		this.password = newUser.password;
+		this.name = newUser.name;
+		this.email = newUser.email;
+		this.userId = newUser.userId;
+		
 	}
 	
 	
