a78ebf7825ef196a182e574383f2f667052af49f
woojin <verecahill@gmail.com>
2019-07-22 15:26:13 +0900

    Added RESTful  API Swagger UI

--- src/main/java/app/BoardApplication.java
+++ src/main/java/app/BoardApplication.java
@@ -3,6 +3,8 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 public class BoardApplication {
 
