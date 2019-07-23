c2d2ba4fb52519f7e066bc167f90e30ce6d59785
woojin <verecahill@gmail.com>
2019-07-23 16:01:30 +0900

    add application-dev.properties setting

--- src/main/java/app/BoardApplication.java
+++ src/main/java/app/BoardApplication.java
@@ -2,14 +2,23 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class BoardApplication {
+public class BoardApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BoardApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
+	
+	
 
 }
